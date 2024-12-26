f05d61564e07977e3bd35b24e1ae913895bb9be1
taeryu7 <taeryu97@gmail.com>
2024-12-26 10:26:01 +0900

    git Ignore 추가

---
+++ BookDataApp/BookDataApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookDataApp
+//
+//  Created by 유태호 on 12/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
